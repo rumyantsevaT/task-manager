333f4e3d4b5508111d2918bff84e174ba5fbd1a8
rum <ipochinili@gmail.com>
2019-03-09 02:33:13 +0300

    123

--- login-form.php
+++ login-form.php
@@ -21,15 +21,15 @@
         <img class="mb-4" src="assets/img/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Авторизация</h1>
         <label for="inputEmail" class="sr-only">Email</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
         <label for="inputPassword" class="sr-only">Пароль</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
         <div class="checkbox mb-3">
           <label>
             <input type="checkbox" value="remember-me"> Запомнить меня
           </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+        <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">Войти</button>
         <a href="register-form.php">Зарегистрироваться</a>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
       </form>
