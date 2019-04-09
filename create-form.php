dc0e9a32e0eecc9203fc940e75736cbfd5a592f9
rumyantsevat <ipochinili@gmail.com>
2019-04-10 00:47:22 +0300

    prepare to create post

--- create-form.php
+++ create-form.php
@@ -1,7 +1,7 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
 
     <title>Create Task</title>
@@ -9,15 +9,15 @@
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
-    
-    <style>
-      
-    </style>
-  </head>
 
-  <body>
-    <div class="form-wrapper text-center">
-      <form class="form-signin">
+    <style>
+
+    </style>
+</head>
+
+<body>
+<div class="form-wrapper text-center">
+    <form class="form-signin">
         <img class="mb-4" src="assets/img/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Добавить запись</h1>
         <label for="inputEmail" class="sr-only">Название</label>
@@ -27,7 +27,7 @@
         <input type="file">
         <button class="btn btn-lg btn-primary btn-block" type="submit">Отправить</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
-      </form>
-    </div>
-  </body>
+    </form>
+</div>
+</body>
 </html>
